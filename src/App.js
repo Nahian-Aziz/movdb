88edec4c50f18c2e2bbf8ed6f41248ecfc992347
Nahian Bin Aziz <nahianbinaziz@Trevor.hitronhub.home>
2018-10-28 00:45:16 -0700

    Fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+    movies: []
+  };
+  async componentDidMount() {
+    try {
+      const result = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=29b9acf7ab2c27f191e212928ff4c160&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+      );
+      const movies = await result.json();
+
+      this.setState({
+        movies: movies.results
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {this.state.movies.map(movie => {
+          return <Movie key={movie.title} movie={movie} />;
+        })}
       </div>
     );
   }
